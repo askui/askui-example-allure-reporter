9aa2723b4ae1bb61a15e392de3032105ace4f2ef
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2023-06-13 23:27:49 +0200

    ci: add pipeline for running tests

--- test/helper/jest.setup.ts
+++ test/helper/jest.setup.ts
@@ -1,28 +1,16 @@
-import { UiControlClient, UiController } from 'askui';
+import { UiControlClient } from 'askui';
 import 'jest-allure-circus';
 import { askuiAllureStepReporter } from './askui-allure-step-reporter';
 import { convertBase64StringToBuffer } from './video-reporting-utils';
 import { ContentType } from "allure-js-commons";
 
 
-// Server for controlling the operating system
-let uiController: UiController;
-
-// Client is necessary to use the askui API
 // eslint-disable-next-line import/no-mutable-exports
 let aui: UiControlClient;
 
 jest.setTimeout(60 * 1000 * 60);
 
 beforeAll(async () => {
-  uiController = new UiController({
-    /**
-     * Select the display you want to run your tests on, display 0 is your main display;
-     * ignore if you have only one display
-     */
-    display: 0,
-  });
-
   aui = await UiControlClient.build({
     credentials: {
       workspaceId: '664e49aa-8f04-49ea-9414-a6fc64b181d3',
