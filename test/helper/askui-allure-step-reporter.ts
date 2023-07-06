969084184e653c6cabfa53ad384d0742f953cebb
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2023-07-06 12:54:23 +0200

    fix attachment label for 'after screenshot'

--- test/helper/askui-allure-step-reporter.ts
+++ test/helper/askui-allure-step-reporter.ts
@@ -1,4 +1,4 @@
-import { Reporter, Step, StepStatus, Annotation, DetectedElement } from "askui";
+import { Reporter, Step, StepStatus, Annotation, DetectedElement, ReporterConfig } from "askui";
 import { Status } from "jest-allure-circus";
 
 function convertPngDataUrlToBuffer(pngDataUrl: string): Buffer {
@@ -20,37 +20,36 @@ function mapAskuiToAllureStepStatus(status: StepStatus): Status {
   }
 }
 
-function createScreenshotOrHTMLAttachment(
+function createScreenshotAttachment(
+  name: string,
   screenshot: string,
-  detectedElements: Readonly<Readonly<DetectedElement>>[])
+  detectedElements?: Readonly<Readonly<DetectedElement>>[])
 {
-  if (detectedElements !== undefined) {
-    const annotation = new Annotation(
-      screenshot,
-      detectedElements
-    );
-    const annotated = annotation.toHtml().serialize(); 
+  if (detectedElements === undefined) {
     return {
-      name: "Before Screenshot with Annotation",
-      type: {
-        contentType: "text/html",
-        fileExtension: ".html",
-      },
-      content: annotated,
-    };
-  } else {
-    return {
-      name: "Before Screenshot",
+      name,
       type: "image/png",
       content: convertPngDataUrlToBuffer(screenshot),
     };
   }
+  const annotation = new Annotation(
+    screenshot,
+    detectedElements
+  );
+  return {
+    name: `${name} (annotated)`,
+    type: {
+      contentType: "text/html",
+      fileExtension: ".html",
+    },
+    content: annotation.toHtml().serialize(),
+  };
 }
 
 export class askuiAllureStepReporter implements Reporter {
-  config = {
-    withScreenshots: 'always' as const,
-    withDetectedElements: 'always' as const,
+  config: ReporterConfig = {
+    withScreenshots: 'onFailure',
+    withDetectedElements: 'onFailure',
   };
 
   async onStepEnd(step: Step): Promise<void> {
@@ -58,13 +57,15 @@ export class askuiAllureStepReporter implements Reporter {
     const attachments = [];
 
     if (step.lastRun?.begin?.screenshot !== undefined) {
-      attachments.push(createScreenshotOrHTMLAttachment(
+      attachments.push(createScreenshotAttachment(
+        "Before Screenshot",
         step.lastRun?.begin?.screenshot,
         step.lastRun?.begin?.detectedElements
       ));
     }
     if (step.lastRun?.end?.screenshot !== undefined) {
-      attachments.push(createScreenshotOrHTMLAttachment(
+      attachments.push(createScreenshotAttachment(
+        "After Screenshot",
         step.lastRun?.end?.screenshot,
         step.lastRun?.end?.detectedElements
       ));
