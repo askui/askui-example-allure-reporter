708fbb7420b93a41038a6536281ea87f663f7541
adi-wan-askui <105295410+adi-wan-askui@users.noreply.github.com>
2023-09-11 09:11:00 +0000

    feat: load config from dotenv

--- test/helper/jest.setup.ts
+++ test/helper/jest.setup.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { UiControlClient } from "askui";
 import { AskUIAllureStepReporter } from "@askui/askui-reporters";
 import { GenericContainer, StartedTestContainer } from 'testcontainers';
@@ -6,16 +7,8 @@ jest.setTimeout(60 * 1000 * 60);
 
 let aui: UiControlClient;
 
-function getDockerImageName(): string {
-  const askuiUiControllerVersion = 'v0.11.2';
-  const browser = 'chrome';
-  const browserVersion = '100.0.4896.60';
-  const osArch = 'amd64';
-  return `askuigmbh/askui-ui-controller:${askuiUiControllerVersion}-${browser}-${browserVersion}-${osArch}`;
-}
-
 async function startTestContainer(): Promise<StartedTestContainer> {
-  const container = new GenericContainer(getDockerImageName())
+  const container = new GenericContainer(process.env["ASKUI_CONTROLLER_DOCKER_IMAGE"])
   const BYTES_PER_GIBIBYTE = 1024 * 1024 * 1024
   const startedContainer = await container.withExposedPorts(6769).withSharedMemorySize(2 * BYTES_PER_GIBIBYTE).start();
   return startedContainer;
@@ -25,8 +18,7 @@ beforeEach(async () => {
   const uiControllerContainer = await startTestContainer();
   aui = await UiControlClient.build({
     inferenceServerUrl:
-      process.env["ASKUI_INFERENCE_SERVER_URL"] ??
-      "https://inference.askui.com",
+      process.env["ASKUI_INFERENCE_SERVER_URL"],
     uiControllerUrl: `http://${uiControllerContainer.getHost()}:${uiControllerContainer.getMappedPort(6769)}`,
     reporter: new AskUIAllureStepReporter({
       withScreenshots: 'always',
@@ -45,7 +37,7 @@ afterEach(async () => {
 
 afterAll(async () => {
   const container = new GenericContainer("tobix/allure-cli");
-  await container.withCommand(["generate", "/allure-results", "-o", "/allure-report"]).withBindMounts([
+  await container.withCommand(["generate", "/allure-results", "--clean", "-o", "/allure-report"]).withBindMounts([
     {
       source: `${process.cwd()}/allure-results`,
       target: '/allure-results',
